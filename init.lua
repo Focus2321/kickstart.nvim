e4e12bf39f4ec07fe3bb174f2e587c726ef6fb27
Hector <hector@langminiserver.tsfd.com>
2025-11-03 19:04:38 +0000

    Added supermaven plugin for autocompletion. Under free plan for now

--- init.lua
+++ init.lua
@@ -350,6 +350,12 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {}
+    end,
+  },
 
   -- NOTE: Plugins can specify dependencies.
   --
